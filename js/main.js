8a074683659ced5b07d35b93ab0117bc5501d34d
Florian Gauger <flo.gauger@gmail.com>
2016-09-28 13:37:59 +0200

    Remove unnecessary code

--- js/main.js
+++ js/main.js
@@ -3,16 +3,33 @@ $(window).on("load", function () {
     var email = "flo.gauger@gmail.com";
     var $window = $(window);
     var $body = $("body");
-    var $intro = $("#intro");
+    var $about = $("#about");
     var $navBar = $('#nav_bar');
     var $navItemContainer = $("#nav_item_container");
-    var $companySelectors = $(".company_selector");
+    var $contact = $('#contact');
+    var $contactForm = $('#contact_form');
+    var $contactEmail = $("#contact_email");
+    var $contactSubject = $('#contact_subject');
+    var $contactMessage = $('#contact_message');
+
+
+    function init() {
+        $window.scroll(scroll);
+        $window.resize(resize);
+        $('.parallax').parallax();
+        $('#home').parallax({imageSrc: 'img/j.jpg'});
+        $('.projects_pic').parallax({imageSrc: 'img/j.jpg'});
+        //$( function () { $('[data-parallax="scroll"]').parallax(); });
+        resize();
+    }
+
+    init();
 
     var navOffsetTop;
 
     function resize() {
         $body.removeClass('nav_docked');
-        navOffsetTop = $intro.offset().top - $navBar.outerHeight();
+        navOffsetTop = $about.offset().top - $navBar.outerHeight();
         scroll();
     }
 
@@ -38,34 +55,14 @@ $(window).on("load", function () {
         location.hash = "#" + hash;
     }
 
-    function init() {
-        $window.scroll(scroll);
-        $window.resize(resize);
-        $('.parallax').parallax();
-        $('#home').parallax({imageSrc: 'img/j.jpg'});
-        $('.projects_pic').parallax({imageSrc: 'img/j.jpg'});
-        //$( function () { $('[data-parallax="scroll"]').parallax(); });
-        resize();
-    }
-
-    init();
-
-
-    var $contact = $('#contact');
-    var $contactForm = $('#contact_form');
-    var $contactEmail = $("#contact_email");
-    var $contactSubject = $('#contact_subject');
-    var $contactMessage = $('#contact_message');
     $contactForm.submit(function (event) {
         sendEmail();
         event.preventDefault();
     });
 
-    /* specify the message that will be displayed if the field is marked invalid */
-    //$contactEmail.get(0).setCustomValidity("Field must be a number.");
 
     function sendEmail() {
-        emailSending();
+        $contact.addClass('sending');
         $.ajax({
             headers: {
                 Accept : "application/json"
@@ -78,14 +75,9 @@ $(window).on("load", function () {
                 message: $contactMessage.val()
             },
             dataType: "json",
-            beforeSend: emailSending,
             success: emailSentSuccess,
             error: emailSentError
         });
-    }
-
-    function emailSending() {
-        $contact.addClass('sending');
     }
 
     function emailSentSuccess() {
@@ -124,163 +116,12 @@ $(window).on("load", function () {
 
     function openEmailClient(email, subject, content) {
         var command = "mailto:" + email;
-        if (subject !== undefined) {
-            command += "?subject=" + subject;
-        }
-        if (content !== undefined) {
-            command += "&body=" + content;
+        var payload = [subject, content].join('&');
+        if (abc.length > 0) {
+            command += "?=" + payload;
         }
         window.location.href = command;
     }
-
-    //var $skills = $(".skill");
-    //$("#default_skill_button").click(function (event) {
-    //    changeSkillTo($skills.eq(Math.floor(Math.random() * $skills.length)));
-    //    event.stopPropagation();
-    //});
-
-    //$skills.click(function (event) {
-    //    changeSkillTo($(this));
-    //    event.stopPropagation();
-    //});
-
-    //$("html").click(contractCurrentSkill);
-
-    //var $currentSkill;
-    //function changeSkillTo($skill) {
-    //    contractCurrentSkill();
-    //    if (!$skill.is($currentSkill)) {
-    //        $currentSkill = $skill;
-    //        expandCurrentSkill();
-    //    } else {
-    //        $currentSkill = undefined;
-    //    }
-    //}
-    //
-    //function expandCurrentSkill() {
-    //    $currentSkill.stop();
-    //    $currentSkill
-    //        .animate({height: "200px"}, 300)
-    //        .css("background-color", "rgba(243, 74, 83, 0.8)");
-    //    $currentSkill.children().css("color", "white");
-    //}
-    //
-    //function contractCurrentSkill() {
-    //    if ($currentSkill) {
-    //        $currentSkill.stop();
-    //        $currentSkill
-    //            .animate({height: "16px"}, 300)
-    //            .css("padding", "")
-    //            .css("background-color", "");
-    //        $currentSkill.children().css("color", "");
-    //    }
-    //}
-
-    //$(function () {
-    //    $("#text1").typed({
-    //        strings: ["Hi! I am Florian.^1000"],
-    //        showCursor: false,
-    //        callback: function () {
-    //            $("#text2").typed({
-    //                strings: ["I like<br>^1000"],
-    //                showCursor: false,
-    //                callback: function () {
-    //                    $("#text3").typed({
-    //                        strings: ["programming", "books", "debating", "learning", "designing", "movies", "handball"],
-    //                        backDelay: 1000,
-    //                        loop: true
-    //                    });
-    //                }
-    //            });
-    //        }
-    //
-    //    });
-    //});
-    //
-    //var $navigation_menu_buttons = $(".navigation_menu_button");
-    //$navigation_menu_buttons.click(function() {
-    //    var nextPage = $navigation_menu_buttons.index(this);
-    //    if (nextPage !== currentPage) {
-    //        changePage(nextPage);
-    //    }
-    //});
-    //
-    //$(".navigation_left").click(function(event) {
-    //    changePage(currentPage - 1);
-    //    event.stopPropagation();
-    //});
-    //$(".navigation_right").click(function(event) {
-    //    changePage(currentPage + 1);
-    //    event.stopPropagation();
-    //});
-    //
-    //
-    //$(document).keydown(function (event) {
-    //    switch (event.which) {
-    //        case 37:
-    //            changePage(currentPage - 1);
-    //            break;
-    //        case 39:
-    //            changePage(currentPage + 1);
-    //            break;
-    //    }
-    //    event.preventDefault();
-    //});
-    //
-    //
-    //var animationEndEventNames = {
-    //    'WebkitAnimation': 'webkitAnimationEnd',
-    //    'OAnimation': 'oAnimationEnd',
-    //    'msAnimation': 'MSAnimationEnd',
-    //    'animation': 'animationend'
-    //};
-    //
-    //var animationEndEventName = animationEndEventNames[Modernizr.prefixed('animation')];
-    //var animating = false;
-    //var currentPage = 0;
-    //var $pages = $(".page");
-    ////var footerColors = ["#F34A53", "#4B515D", "#4B515D"];
-    //
-    //var prevOutClass = 'page-rotateLeftSideFirst';
-    //var prevInClass = 'page-moveFromLeft page-delay200 page-ontop';
-    //var nextOutClass = 'page-rotateRightSideFirst';
-    //var nextInClass = 'page-moveFromRight page-delay200 page-ontop';
-    //
-    //
-    //function changePage(nextPage) {
-    //    if (!animating && nextPage >= 0 && nextPage < $pages.length) {
-    //        var $current = $pages.eq(currentPage);
-    //        var $next = $pages.eq(nextPage);
-    //
-    //        if (animationEndEventName) {
-    //            animating = true;
-    //            $navigation_menu_buttons.eq(currentPage).removeClass("current");
-    //            $navigation_menu_buttons.eq(nextPage).addClass("current");
-    //
-    //            var outClass = nextPage > currentPage ? nextOutClass : prevOutClass;
-    //            var inClass = nextPage > currentPage ? nextInClass : prevInClass;
-    //            currentPage = nextPage;
-    //
-    //            //$("#footer").css("background-color", footerColors[currentPage]);
-    //            var firstCallback = true;
-    //            $current.addClass(outClass).off(animationEndEventName).on(animationEndEventName, endAnimation);
-    //            $next.addClass(inClass + " page-current").off(animationEndEventName).on(animationEndEventName, endAnimation);
-    //
-    //            function endAnimation() {
-    //                if (firstCallback) {
-    //                    firstCallback = false;
-    //                } else {
-    //                    $current.removeClass(outClass + " page-current");
-    //                    $next.removeClass(inClass);
-    //                    animating = false;
-    //                }
-    //            }
-    //        } else {
-    //            $current.removeClass("page-current");
-    //            $next.addClass("page-current")
-    //        }
-    //    }
-    //}
 });
 
 
