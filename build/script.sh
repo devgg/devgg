deaba5956360642bd789087d240cdc39d3e54f75
Florian Gauger <flo.gauger@gmail.com>
2016-09-29 15:11:32 +0200

    Change build step

--- build/script.sh
+++ build/script.sh
@@ -38,8 +38,9 @@ rsync -av --exclude='.git' \
 
 
 git clone https://github.com/devgg/FontIcon.git fonticon
-rsync -av --exclude='.gitignore' \
-          --exclude='README.md' \
+rsync -av --exclude='fonticon/.git' \
+          --exclude='fonticon/.gitignore' \
+          --exclude='fonticon/README.md' \
           fonticon out/fonticon
 
 
